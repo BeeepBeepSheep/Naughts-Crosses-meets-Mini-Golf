1951cf7b8cb2ec2759f7035a4d765bb5d041a0f6
BeeepBeepSheep <jacoblueke@gmail.com>
2021-09-12 16:37:53 +0100

    got drag power working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,19 @@ using System.Collections;using System.Collections.Generic;using UnityEngine;u
     }    void Start()    {        redBall.useGravity = false;        powerSlider.maxValue = maxShotPower;    }    void Update()    {        transform.position = currantBall.position;
 
         //ui
-        speedDial.SetText(currantBall.velocity.magnitude.ToString());        powerDial.SetText(currantShotPower.ToString());        powerSlider.value = currantShotPower;        xRotation += Input.GetAxis("Mouse X") * rotationSpeed;
+        speedDial.SetText(currantBall.velocity.magnitude.ToString());        powerDial.SetText(currantShotPower.ToString());
+        powerSlider.value = currantShotPower;
+        xRotation += Input.GetAxis("Mouse X") * rotationSpeed;
 
         // stop movement if to slow
         if (currantBall.velocity.magnitude <= 0.05)        {            stopVelocity = new Vector3(0, 0, 0);            currantBall.velocity = stopVelocity;            SwitchPlayer();        }
 
         //rotate cam
-        transform.rotation = Quaternion.Euler(transform.rotation.y, xRotation, 0f);        ShootLogic();    }    void ShootLogic()    {        if (canShoot)        {            if (Input.GetMouseButtonDown(1))
-            {
-                currantShotPower = 0f;
-            }            if (Input.GetMouseButton(0))            {                if (currantShotPower < maxShotPower)
+        transform.rotation = Quaternion.Euler(transform.rotation.y, xRotation, 0f);        ShootLogic();    }    void ShootLogic()    {        if (canShoot)        {            if (Input.GetMouseButton(0))            {                currantShotPower -= Input.GetAxis("Mouse Y");                if (currantShotPower > maxShotPower)
                 {
-                    currantShotPower += 30 * Time.deltaTime;
+                    currantShotPower = maxShotPower;
+                }
+                if (currantShotPower < 0f)
+                {
+                    currantShotPower = 0f;
                 }            }            if (Input.GetMouseButtonUp(0))            {                currantBall.velocity = transform.forward * (currantShotPower);                currantShotPower = 0f;                canSwitch = true;                canShoot = false;            }        }    }    void SwitchPlayer()    {        if (canSwitch & currantBall.velocity.x == 0 & currantBall.velocity.y == 0 & currantBall.velocity.z == 0)        {            canShoot = true;            canSwitch = false;            currantShotPower = 0f;            if (currantBall == blueBall)            {                currantBall = redBall;                currantBall.useGravity = true;            }            else            {                currantBall = blueBall;            }        }    }}
